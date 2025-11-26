de094dd242d6e5f8a0f3e0bf2f3727e5a5b63e6c
Giada <tonni.giada.210802@gmail.com>
2025-11-26 13:11:44 +0100

    COPIA VENERDI FATTA

---
+++ progetto_settimanale_s9/src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Home from './components/Home'
+import Login from './components/Login'
+import Settings from './components/Settings'
+
+function App() {
+  return (
+    <>
+      <Login />
+      <Home />
+      <Settings />
+    </>
+  )
+}
+
+export default App
